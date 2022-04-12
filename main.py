3af2867e9f833056e9244cb6634d63baf0ddb756
sasha <gagabuba@mail.ru>
2022-04-12 23:12:09 +0300

    Done second function

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
 import click
-from datetime import date
-from datetime import time
 from datetime import datetime
-from datetime import timedelta
+import enum
+import random
 
-#@click.command()
-#@click.option('--hour')
 
-def newyear():
-    today = date.today()
-    nyd = date(today.year + 1, 1, 1)
-    print((nyd - today).days)
+@click.group()
+def cli():
+    pass
 
-if __name__ == '__main__':
-    newyear()
+@cli.command()
+@click.option('--hour', is_flag=True, default=False)
+def newyear(hour):
+    today = datetime.today()
+    nyd = datetime(today.year + 1, 1, 1, 0, 0, 0)
+    nydd = nyd - today
+    if hour:
+        click.echo(f'До Нового года {nydd.days} д. и {int(nydd.seconds/3600)} ч.')
+    else:
+        click.echo(f'До нового года {nydd.days} дней')
 
+@cli.command()
+def toy():
+    colour = ['red', 'green', 'yellow', 'blue', 'purple']
+    toys = ['star', 'ball', 'angel', 'snowflake']
+    click.echo(f'{random.choice(colour)} {random.choice(toys)}')
+    
+
+if __name__ == "__main__":
+    cli()
